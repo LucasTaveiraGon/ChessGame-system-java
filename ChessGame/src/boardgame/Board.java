be086a9449dbf849d243e792de87596db4b89319
Lucas Taveira <lucastaveirag@gmail.com>
2022-03-01 17:13:38 -0300

    Chess Layer and PrintBoard

--- ChessGame/src/boardgame/Board.java
+++ ChessGame/src/boardgame/Board.java
@@ -20,6 +20,14 @@ public class Board {
         pieces  = new Piece[rows][columns];
     }
 
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
     public int getColumns() {
         return columns;
     }
@@ -36,5 +44,11 @@ public class Board {
         this.pieces = pieces;
     }
     
+    public Piece piece(int row, int column){
+        return pieces[row][column];
+    }
     
+    public Piece piece(Position position){
+        return pieces[position.getRow() ][position.getColumn() ];
+    }
 }
