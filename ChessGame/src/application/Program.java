527aa70effc95bd203cec16572da598d521cc843
Lucas Taveira <lucastaveirag@gmail.com>
2022-03-01 15:52:53 -0300

    created Projec and Position Class

---
+++ ChessGame/src/application/Program.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package application;
+
+import boardgame.Position;
+
+/**
+ *
+ * @author lucas
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Position position = new Position(3,5);
+        System.out.println(position);
+    }
+    
+}
