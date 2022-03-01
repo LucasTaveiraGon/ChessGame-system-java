24e88273140a77f3f534f8f0a2d63029d41288bd
Lucas Taveira <lucastaveirag@gmail.com>
2022-03-01 16:23:33 -0300

    create Board and Piece Class

--- ChessGame/src/application/Program.java
+++ ChessGame/src/application/Program.java
@@ -4,6 +4,7 @@
  */
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 /**
@@ -17,6 +18,7 @@ public class Program {
      */
     public static void main(String[] args) {
         Position position = new Position(3,5);
+        Board board = new Board(8,8);
         System.out.println(position);
     }
     
---
+++ ChessGame/src/boardgame/Piece.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boardgame;
+
+/**
+ *
+ * @author lucas
+ */
+public class Piece {
+    
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        position = null;
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    
+    
+}
