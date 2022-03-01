bcac790282788d6e25ebe3a134bfb4935dbf6f0c
Lucas Taveira <lucastaveirag@gmail.com>
2022-03-01 17:46:00 -0300

    King, Rook and PlacePieces

--- ChessGame/src/chess/pieces/Rook.java
+++ ChessGame/src/chess/pieces/Rook.java
@@ -2,26 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package chess;
+package chess.pieces;
 
 import boardgame.Board;
-import boardgame.Piece;
+import chess.ChessPiece;
+import chess.Color;
 
 /**
  *
  * @author lucas
  */
-public class ChessPiece extends Piece {
-    private  Color color;
-
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
+public class Rook extends ChessPiece {
+    
+    public Rook(Board board,Color color) {
+        super(board, color);
+        
     }
 
-    public Color getColor() {
-        return color;
-    }
 
+    @Override
+    public String toString() {
+        return "R";
+    }
+    
     
 }
